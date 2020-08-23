d109ee2e1275512fbd3172244b7bdaf206c0fa6f
Manuel <pfandlmanuel@protonmail.com>
2020-08-23 09:54:33 +0200

    Return current Celsius and Humidity

--- weather.py
+++ weather.py
@@ -1,6 +1,7 @@
 import configparser
 import requests
 from pprint import pprint
+import json
 
 #read the config.txt
 config = configparser.ConfigParser()
@@ -13,8 +14,22 @@ base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
 # API call
 Final_url = base_url + "q=" + city_name + "&appid=" + API_key
-print(Final_url)
-weather_data = requests.get(Final_url).json()
+#print(Final_url)
 
 # Callback
-pprint(weather_data)
+weather_data = requests.get(Final_url).json()
+ktemp = weather_data["main"]["temp"]
+humidity = weather_data["main"]["humidity"]
+
+
+#Convert Fahrenheit to Celsius
+ctemp = ktemp - 273.15
+
+print(int(ctemp), "C°")
+print(humidity, "%")
+
+# Parse JSON
+#parsed_data = (json.loads(weather_data))
+#print(parsed_data)
+#for x in parsed_data:
+#	print("%s: %s" % (x, parsed_data[x]))

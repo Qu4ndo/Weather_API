14971624f1fb9e47efc23c62c7eb933cfeb7f0c8
Manuel <pfandlmanuel@protonmail.com>
2020-08-23 10:41:49 +0200

    Cleanup Codebase and included a few functions

--- weather.py
+++ weather.py
@@ -9,29 +9,42 @@ def get_callback():
     API_key = config.get('Basic-Configuration', 'API_key')
     city_name = config.get('Basic-Configuration', 'city_name')
 
-    # API call
+    # API
     base_url = "http://api.openweathermap.org/data/2.5/weather?"
     Final_url = base_url + "q=" + city_name + "&appid=" + API_key
 
-    # Callback
-    weather_data = requests.get(Final_url).json()
-
-    return weather_data
+    # API call
+    return requests.get(Final_url).json()
 
 
-weather_data = get_callback()
-print(weather_data)
+def convert_kelvin(ktemp):
+    # Convert Kelvin to Celsius
+    return ktemp - 273.15
 
-ktemp = weather_data["main"]["temp"]
-humidity = weather_data["main"]["humidity"]
-wind_ms = weather_data["wind"]["speed"]
 
-#Convert Kelvin to Celsius
-ctemp = ktemp - 273.15
-#Convert Windspeed to km/h
-wind = wind_ms * 3.6
+def convert_kmh(wind_ms):
+    # Convert Windspeed to km/h
+    return wind_ms * 3.6
 
-#Print Data
-print(int(ctemp), "C°")
-print(humidity, "%")
-print(int(wind), "km/h")
+
+def parse_values(weather_data):
+    # Parsing different values
+    ktemp = weather_data["main"]["temp"]
+    humidity = weather_data["main"]["humidity"]
+    wind_ms = weather_data["wind"]["speed"]
+
+    # Convert Values
+    ctemp = convert_kelvin(ktemp)
+    wind = convert_kmh(wind_ms)
+
+    return ctemp, humidity, wind
+
+
+if __name__=="__main__":
+    weather_data = get_callback()
+    ctemp, humidity, wind = parse_values(weather_data)
+
+    # Print Data
+    print(int(ctemp), "C°")
+    print(humidity, "%")
+    print(int(wind), "km/h")

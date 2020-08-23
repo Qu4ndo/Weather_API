da8d5e3a4fb5943e63ca2a9b531eb1fccbc4e150
Manuel <pfandlmanuel@protonmail.com>
2020-08-23 10:17:24 +0200

    Put API Call in function

--- weather.py
+++ weather.py
@@ -3,26 +3,31 @@ import requests
 from pprint import pprint
 import json
 
-#read the config.txt
-config = configparser.ConfigParser()
-config.read_file(open(r'config.txt'))
-API_key = config.get('Basic-Configuration', 'API_key')
-city_name = config.get('Basic-Configuration', 'city_name')
+def get_callback():
+    #read the config.txt
+    config = configparser.ConfigParser()
+    config.read_file(open(r'config.txt'))
+    API_key = config.get('Basic-Configuration', 'API_key')
+    city_name = config.get('Basic-Configuration', 'city_name')
 
-# URL for API call
-base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    # API call
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    Final_url = base_url + "q=" + city_name + "&appid=" + API_key
 
-# API call
-Final_url = base_url + "q=" + city_name + "&appid=" + API_key
-#print(Final_url)
+    # Callback
+    weather_data = requests.get(Final_url).json()
 
-# Callback
-weather_data = requests.get(Final_url).json()
+    return weather_data
+
+
+weather_data = get_callback()
+#print(weather_data)
+
 ktemp = weather_data["main"]["temp"]
 humidity = weather_data["main"]["humidity"]
 
 
-#Convert Fahrenheit to Celsius
+#Convert Kelvin to Celsius
 ctemp = ktemp - 273.15
 
 print(int(ctemp), "C°")
